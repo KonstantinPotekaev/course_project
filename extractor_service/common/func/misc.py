359afda8d731714362f0e6cc70be5c81f3b63b00
Konstantin Potekaev <Konstantin.Potekaev@infowatch.com>
2025-03-11 14:38:12 +0300

    + misc.py: Убран неиспользуемый класс;

--- extractor_service/common/func/misc.py
+++ extractor_service/common/func/misc.py
@@ -7,12 +7,3 @@ class S3ContentType(str, Enum):
     JSON = "application/json"
     TEXT = "text/plain"
     OCTET_STREAM = "application/octet-stream"
-
-
-class S3ObjectInfo(BaseModel):
-    bucket_name: str
-    object_key: str
-
-    def __hash__(self):
-        return hash(f"{self.bucket_name}:{self.object_key}")
-
