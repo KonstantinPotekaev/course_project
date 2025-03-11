ce1f3c39054d2715f129da9a2375a6fe3b7be634
Konstantin Potekaev <Konstantin.Potekaev@infowatch.com>
2025-03-11 14:39:31 +0300

    + wem/common/env/*: Добавлены переменные окружения;

---
+++ web/common/env/general.py
@@ -0,0 +1,8 @@
+import os
+
+AES_HOST = os.getenv("AES_HOST")
+
+S3_ENDPOINT_URL = os.getenv("S3_ENDPOINT_URL")
+S3_ACCESS_KEY = os.getenv("S3_ACCESS_KEY")
+S3_SECRET_KEY = os.getenv("S3_SECRET_KEY")
+S3_BUCKET = os.getenv("S3_BUCKET")
---
+++ web/common/env/resources.py
@@ -0,0 +1,4 @@
+import os
+
+CHUNK_SIZE = os.getenv('CHUNK_SIZE', 1024)
+THREADS = os.getenv('THREADS', 1)
